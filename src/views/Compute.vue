650b6b861498584bc8cbea3a948aa9922c465474
Ked57 <kedketh@gmail.com>
2019-05-14 09:02:41 +0200

    Updated layout to align center everything

--- src/views/Compute.vue
+++ src/views/Compute.vue
@@ -1,80 +1,83 @@
 <template>
   <section class="section">
     <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-narrow">
-          <h1 class="title">Calculatrice</h1>
-          <button
-            class="accordion"
-            v-on:click="discountRatePanel = !discountRatePanel"
-          >Taux de remise</button>
-          <div class="panel" v-show="discountRatePanel">
-            <span>Prix d'achat net</span>
-            <input type="text" class="input" v-model="operatingPurchasePrice">
-            <span>Prix d'achat brut</span>
-            <input type="text" class="input" v-model="grossPurchasePrice">
-            <input type="submit" class="submit" value="Calculer" v-on:click="computeDiscountRate">
-            <span
-              class="result"
-              v-show="discountRate"
-            >Résultat: {{ parseFloat(this.discountRate).toFixed(2) }} %</span>
-          </div>
-          <button
-            class="accordion"
-            v-on:click="operatingPurchasePricePanel = !operatingPurchasePricePanel"
-          >Prix d'achat net</button>
-          <div class="panel" v-show="operatingPurchasePricePanel">
-            <span>Prix d'achat brut</span>
-            <input type="text" class="input" v-model="grossPurchasePrice">
-            <span>Taux de remise</span>
-            <input type="text" class="input" v-model="discountRate">
-            <input
-              type="submit"
-              class="submit"
-              value="Calculer"
-              v-on:click="computeOperatingPurchasePrice"
-            >
-            <span
-              class="result"
-              v-show="operatingPurchasePrice"
-            >Résultat: {{ parseFloat(this.operatingPurchasePrice).toFixed(2) }}</span>
-          </div>
-          <button
-            class="accordion"
-            v-on:click="operatingSellPricePanel = !operatingSellPricePanel"
-          >Prix de vente net</button>
-          <div class="panel" v-show="operatingSellPricePanel">
-            <span>Prix d'achat net</span>
-            <input type="text" class="input" v-model="operatingPurchasePrice">
-            <span>Coefficient multiplicateur</span>
-            <input type="text" class="input" v-model="multiplier">
-            <input
-              type="submit"
-              class="submit"
-              value="Calculer"
-              v-on:click="computeOperatingSellPrice"
-            >
-            <span
-              class="result"
-              v-show="operatingSellPrice"
-            >Résultat: {{ parseFloat(this.operatingSellPrice).toFixed(2) }}</span>
-          </div>
-          <button
-            class="accordion"
-            v-on:click="multiplierPanel = !multiplierPanel"
-          >Coefficient multiplicateur</button>
-          <div class="panel" v-show="multiplierPanel">
-            <span>Prix de vente net</span>
-            <input type="text" class="input" v-model="operatingSellPrice">
-            <span>Prix d'achat net</span>
-            <input type="text" class="input" v-model="operatingPurchasePrice">
-            <input type="submit" class="submit" value="Calculer" v-on:click="computeMultiplier">
-            <span
-              class="result"
-              v-show="multiplier"
-            >Résultat: {{ parseFloat(this.multiplier).toFixed(2) }}</span>
-          </div>
-        </div>
+      <h1 class="title is-text-align-center">Calculatrice</h1>
+      <button class="accordion" v-on:click="discountRatePanel = !discountRatePanel">Taux de remise</button>
+      <div class="panel is-vertical-center" v-show="discountRatePanel">
+        <span>Prix d'achat net</span>
+        <input type="text" class="formElement" v-model="operatingPurchasePrice">
+        <span>Prix d'achat brut</span>
+        <input type="text" class="formElement" v-model="grossPurchasePrice">
+        <input
+          type="submit"
+          class="submit formElement"
+          value="Calculer"
+          v-on:click="computeDiscountRate"
+        >
+        <span
+          class="result"
+          v-show="discountRate"
+        >Résultat: {{ parseFloat(this.discountRate).toFixed(2) }} %</span>
+      </div>
+      <button
+        class="accordion"
+        v-on:click="operatingPurchasePricePanel = !operatingPurchasePricePanel"
+      >Prix d'achat net</button>
+      <div class="panel is-vertical-center" v-show="operatingPurchasePricePanel">
+        <span>Prix d'achat brut</span>
+        <input type="text" class="formElement" v-model="grossPurchasePrice">
+        <span>Taux de remise</span>
+        <input type="text" class="formElement" v-model="discountRate">
+        <input
+          type="submit"
+          class="submit formElement"
+          value="Calculer"
+          v-on:click="computeOperatingPurchasePrice"
+        >
+        <span
+          class="result"
+          v-show="operatingPurchasePrice"
+        >Résultat: {{ parseFloat(this.operatingPurchasePrice).toFixed(2) }}</span>
+      </div>
+      <button
+        class="accordion"
+        v-on:click="operatingSellPricePanel = !operatingSellPricePanel"
+      >Prix de vente net</button>
+      <div class="panel is-vertical-center" v-show="operatingSellPricePanel">
+        <span>Prix d'achat net</span>
+        <input type="text" class="formElement" v-model="operatingPurchasePrice">
+        <span>Coefficient multiplicateur</span>
+        <input type="text" class="formElement" v-model="multiplier">
+        <input
+          type="submit"
+          class="submit formElement"
+          value="Calculer"
+          v-on:click="computeOperatingSellPrice"
+        >
+        <span
+          class="result"
+          v-show="operatingSellPrice"
+        >Résultat: {{ parseFloat(this.operatingSellPrice).toFixed(2) }}</span>
+      </div>
+      <button
+        class="accordion"
+        v-on:click="multiplierPanel = !multiplierPanel"
+      >Coefficient multiplicateur</button>
+      <div class="panel is-vertical-center" v-show="multiplierPanel">
+        <span>Prix de vente net</span>
+        <input type="text" class="formElement" v-model="operatingSellPrice">
+        <span>Prix d'achat net</span>
+        <input type="text" class="formElement" v-model="operatingPurchasePrice">
+        <input
+          type="submit"
+          class="submit formElement"
+          value="Calculer"
+          v-on:click="computeMultiplier"
+        >
+        <span
+          class="result"
+          v-show="multiplier"
+        >Résultat: {{ parseFloat(this.multiplier).toFixed(2) }}</span>
       </div>
     </div>
   </section>
@@ -115,7 +118,7 @@ export default {
 </script>
 
 <style scoped>
-.input {
+.formElement {
   max-width: 400px;
 }
 .accordion {
@@ -127,9 +130,14 @@ export default {
   text-align: left;
   border: none;
   outline: none;
-  transition: 0.4s;
 }
-
+.is-vertical-center {
+  display: flex;
+  align-items: center;
+}
+.is-text-align-center {
+  text-align: center;
+}
 .active,
 .accordion:hover {
   background-color: #ccc;
@@ -142,5 +150,6 @@ export default {
   flex-direction: column;
   overflow: hidden;
   justify-content: center;
+  transition: 0.5s;
 }
 </style>
