512bd79a5d8ebe23018e35900ff67f5398947bda
Alex-Corbel <alexcorbel97@gmail.com>
2019-05-20 14:01:49 +0200

    Création formulaire Note

---
+++ src/views/UpdateNote.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title is-text-align-center">Ajout/Modification d'un commentaire</h1>
+      <h2 class="title is-text-align-center">Nom de la Pharmacie : Le nom sera affiché ici</h2>
+      <div id="closeMe" class="notification is-danger" v-if="errors.length">
+        <button class="delete" @click="closeBlock"></button>
+        <b>Corriger les erreurs suivantes :</b>
+        <ul>
+          <li v-for="error in errors" :key="error.message">{{ error }}</li>
+        </ul>
+      </div>
+
+      <label class="label" for="info">Information souhaitée</label>
+      <select v-model="info" class="input is-text-align-center formElement">
+        <option
+          v-for="option in options"
+          :key="option.text"
+          v-bind:value="option.value"
+        >{{ option.text }}</option>
+      </select>
+
+      <label class="label" for="note">Note</label>
+      <textarea
+        class="input is-text-align-center formElement"
+        v-model="note"
+        placeholder="add multiple lines"
+      ></textarea>
+      <br>
+      <input class="button is-link" type="submit" value="Valider" @click="submitForm">
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "UpdateNote",
+  data() {
+    return {
+      errors: [],
+      options: [
+        { text: "One", value: "A" },
+        { text: "Two", value: "B" },
+        { text: "Three", value: "C" }
+      ],
+      info: undefined,
+      note: undefined,
+      pharmacyId: undefined
+    };
+  },
+  methods: {
+    checkForm: function(e) {
+      if (this.info && this.note) {
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.info) {
+        this.errors.push({
+          message: "Le choix du type d'information est requis."
+        });
+      }
+      if (!this.adress) {
+        this.errors.push({ message: "L'information est requise." });
+      }
+      if (this.errors.length > 0) {
+        this.displayErrors = true;
+      }
+    },
+    closeBlock() {
+      this.displayErrors = false;
+    },
+    submitForm() {
+      this.checkForm();
+      console.log("form is submitted");
+    }
+  },
+  mounted() {
+    this.id = this.$route.params.id;
+    if (!this.id || this.id === "") {
+      console.error("no id provided");
+    }
+  }
+};
+</script>
+<style>
+.is-text-align-center {
+  text-align: center;
+}
+.formElement {
+  max-width: 400px;
+}
+.is-vertical-center {
+  display: flex;
+  align-items: center;
+}
+</style>
