cdb938c010758a9cb1eebfcafb5c4398665828ee
Ked57 <kedketh@gmail.com>
2019-06-07 11:07:51 +0200

    new eslint rules to work better with vuejs and using axios as a fetcher now so the fetch works in android

--- src/api/fetcher.js
+++ src/api/fetcher.js
@@ -1,18 +1,21 @@
+import axios from "axios";
+
 const BASE_URL = "http://shyndard.eu:8080/api";
-// eslint-disable-next-line space-before-function-paren
+
 export const fetchApi = async (method, url, token, args) => {
-  const response = await fetch(`${BASE_URL}/${url}`, {
+  const response = await axios({
+    url: `${BASE_URL}/${url}`,
     method: method,
     headers: {
       authorization: `Bearer ${token}`
     },
-    body: JSON.stringify(args)
+    data: JSON.stringify(args)
   });
   if (!response) {
     throw new Error("Aucune réponse du serveur");
   }
   if (response.status >= 400) {
-    throw new Error((await response.json()).error);
+    throw new Error(response.statusText);
   }
-  return response.json();
+  return response.data;
 };
